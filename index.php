7e502e2730536f14d2a9d32032e6e4bdc11268ee
dafec <75421080+dafec@users.noreply.github.com>
2020-12-03 14:10:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?PHP 
-
-$client = new MongoDB\Client(
-    'mongodb+srv://dafec-admin:M1-apPle-Silicon@dafec.y0s1i.mongodb.net/AFECWEB?retryWrites=true&w=majority');
-
-$db = $client->test;
-
 echo "This is my index page";
 ?>
