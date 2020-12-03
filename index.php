c38f488a277c538a725206b589cb8264b3222740
dafec <75421080+dafec@users.noreply.github.com>
2020-12-03 14:03:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?PHP 
+
+$client = new MongoDB\Client(
+    'mongodb+srv://dafec-admin:M1-apPle-Silicon@dafec.y0s1i.mongodb.net/<dbname>?retryWrites=true&w=majority');
+
+$db = $client->test;
+
 echo "This is my index page";
 ?>
